ce41cf66ad496ecbe41bf2e83aeba9bd959be577
vanegomez <vane.gomez@me.com>
2015-09-15 16:02:22 -0600

    script borrower creates a loan request

--- lib/load_script/session.rb
+++ lib/load_script/session.rb
@@ -46,12 +46,20 @@ module LoadScript
     end
 
     def actions
-      [:lender_makes_loan ]
-      # [:sign_up_as_lender, :sign_up_as_borrower, :browse_loan_requests, :browse_loan_requests_pages, :browse_categories, :browse_categories_pages, :view_a_loan_request, :lender_makes_loan ]
+      [:borrower_creates_loan_request]
+      # [:sign_up_as_lender, :sign_up_as_borrower, :browse_loan_requests, :browse_loan_requests_pages, :browse_categories, :browse_categories_pages, :view_a_loan_request, :lender_makes_loan, :borrower_creates_loan_request ]
     end
 
     def categories
       ["Agriculture", "Education", "Community"]
+    end
+
+    def new_user_name
+      "#{Faker::Name.name} #{Time.now.to_i}"
+    end
+
+    def new_user_email(name)
+      "TuringPivotBots+#{name.split.join}@gmail.com"
     end
 
     def log_in(email="demo+horace@jumpstartlab.com", pw="password")
@@ -105,14 +113,6 @@ module LoadScript
       puts "Viewing an individual loan request"
     end
 
-    def new_user_name
-      "#{Faker::Name.name} #{Time.now.to_i}"
-    end
-
-    def new_user_email(name)
-      "TuringPivotBots+#{name.split.join}@gmail.com"
-    end
-
     def sign_up_as_lender(name = new_user_name)
       log_out
       session.find("#sign-up-dropdown").click
@@ -138,7 +138,7 @@ module LoadScript
         session.fill_in("user_password_confirmation", with: "password")
         session.click_link_or_button "Create Account"
       end
-      puts "Sign-up as a Borrower"
+      puts "Sign-up as a new Borrower"
     end
 
     def lender_makes_loan
@@ -149,6 +149,22 @@ module LoadScript
       session.click_link_or_button("Transfer Funds")
       log_out
       puts "Lender makes a loan"
+    end
+
+    def borrower_creates_loan_request
+      log_out
+      sign_up_as_borrower
+      session.click_link_or_button("Create Loan Request")
+      session.within("#loanRequestModal") do
+        session.fill_in("loan_request_title", with: Faker::Commerce.product_name)
+        session.fill_in("loan_request_description", with: Faker::Company.catch_phrase)
+        session.fill_in("loan_request_requested_by_date", with: Faker::Time.between(7.days.ago, 3.days.ago))
+        session.fill_in("loan_request_repayment_begin_date", with: Faker::Time.between(3.days.ago, Time.now))
+        session.select("Agriculture", from: "loan_request_category")
+        session.fill_in("loan_request_amount", with: "200")
+        session.click_link_or_button("Submit")
+      end
+      puts "Borrower creates a loan request"
     end
   end
 end
